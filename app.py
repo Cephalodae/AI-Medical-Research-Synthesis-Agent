29230799f61801b774ff69feaa7a03c54c7c5ea6
Ryan Scott <35160393+UnlitRyun@users.noreply.github.com>
2025-11-11 13:04:00 -0800

    +basic prompt page +basic results page

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def initial_page():
+@app.route('/', methods=['GET', 'POST'])
+def documents_page():
     return render_template('main.html')
 
-@app.route('/documents')
-def documents_page():
-    return 'Document view'
+@app.route('/results', methods=['GET', 'POST'])
+def results_page():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run()
