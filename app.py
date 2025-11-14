b561e77619c87aabe40ed5a3b15b7ca110960978
Ryan Scott <35160393+UnlitRyun@users.noreply.github.com>
2025-11-13 16:38:08 -0800

    +pdf text extraction +ui adjustments
    Extracted text just gets put into results box atm. Only pdf files are accepted and having a file is required.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import fitz
+from flask import Flask, render_template, request, redirect, url_for
 import time
 
 app = Flask(__name__)
@@ -7,9 +8,20 @@ app = Flask(__name__)
 def documents_page():
     return render_template('main.html')
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route('/results', methods=['POST'])
 def results_page():
-    return render_template('results.html')
+    pdfs = request.files.getlist('input_docu')
+
+    extracted_text = ""
+    for pdf in pdfs:
+        pdf_convert = pdf.read()
+
+        fitz_pdf = fitz.open(stream=pdf_convert)
+        for page in fitz_pdf:
+            extracted_text += page.get_text()
+        fitz_pdf.close()
+
+    return render_template('results.html', extracted_text=extracted_text)
 
 if __name__ == '__main__':
     app.run()
