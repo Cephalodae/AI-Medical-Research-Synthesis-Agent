c40c8db7c51924178f875b394e02b4fcdbcf16a4
Raunak Chitre <raunakchitre@Raunaks-MacBook-Pro.local>
2025-11-14 14:26:21 -0500

    rest of rag and vector database

--- rag.py
+++ rag.py
@@ -5,11 +5,26 @@ from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from ibm_watsonx_ai import Credentials
 from langchain_ibm import WatsonxEmbeddings
+from langchain_ibm import WatsonxLLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 
 key_path = "./PRIVATE/key.txt"
 project_path = "./PRIVATE/project.txt"
 
+if "IBM_API_KEY" not in os.environ:
+    with open(key_path, "r") as f:
+        os.environ["IBM_API_KEY"] = f.read()
+
+if "IBM_PROJECT_ID" not in os.environ:
+    with open(project_path, "r") as f:
+        os.environ["IBM_PROJECT_ID"] = f.read()
+
+os.environ.setdefault("IBM_URL", "https://us-south.ml.cloud.ibm.com")
+os.environ.setdefault("IBM_EMBED_MODEL", "ibm/granite-embedding-107m-multilingual")
+
 def get_embeddings():
+    
     api_key = os.getenv("IBM_API_KEY")
     url = os.getenv("IBM_URL", "https://us-south.ml.cloud.ibm.com")
     project_id = os.getenv("IBM_PROJECT_ID")
@@ -51,3 +66,42 @@ def retrieve_once(paths, query, k=4):
     if not vs:
         return []
     return vs.similarity_search(query, k=k)
+
+def get_llm():
+    model_id = "ibm/granite-13b-chat-v2" 
+    api_key    = os.environ["IBM_API_KEY"]
+    project_id = os.environ["IBM_PROJECT_ID"]
+    creds = Credentials(url="https://us-south.ml.cloud.ibm.com", api_key=api_key)
+
+    return WatsonxLLM(credentials=creds, project_id=project_id, model_id=model_id, params={
+            "max_new_tokens": 400,
+            "temperature": 0.2,
+            "top_p": 0.9,
+        },)    
+
+
+system_prompt = """You are a careful research assistant.
+Answer ONLY using the supplied context between <<<CONTEXT>>> and <<<END>>>.
+If the answer is not present, say "I don't know."
+Be concise and factual."""
+
+template = PromptTemplate.from_template(
+    system_prompt + "\n\n<<<CONTEXT>>>\n{context}\n<<<END>>>\n\nQuestion: {question}\nAnswer:"
+)
+
+def get_answer(paths, query):
+    vs = create_vector_store(paths)
+    if not vs:
+        return "No documents provided."
+
+    llm = get_llm()
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vs.as_retriever(search_kwargs={"k": 4}),
+        chain_type_kwargs={"prompt": template},
+        return_source_documents=True,
+    )
+
+    result = qa_chain({"query": query})
+    return result
